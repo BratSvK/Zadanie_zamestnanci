8720e4aefbcf3ee16ee7b9c2f94bfc7d16c7fb2b
Martin Gajdos <martingajdos.zs@gmail.com>
2021-08-18 13:21:34 +0200

    edit base controller

---
+++ CompanyManagerAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
